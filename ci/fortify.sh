da3f7fc32013beaeda5b5b84a6b3536cf9fa4e16
Ben Hosmer <ben.hosmer@gmail.com>
2016-09-30 11:39:05 -0400

    Fix file not found

--- ci/fortify.sh
+++ ci/fortify.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+pushd `dirname $0`/.. > /dev/null
+root=$(pwd -P)
 echo scanning 
 # Fortify Scan
-echo 'Building $BUILD_NUMBER'
-/opt/hp_fortify_sca/bin/sourceanalyzer -b $BUILD_NUMBER ../src/main/
+echo 'Building' $BUILD_NUMBER
+/opt/hp_fortify_sca/bin/sourceanalyzer -b $BUILD_NUMBER $root/src
+
+popd > /dev/null
 
