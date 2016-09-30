ae7caed6c274f0647e39d84d00d63fb03bd4eefe
Ben Hosmer <ben.hosmer@gmail.com>
2016-09-30 11:10:30 -0400

    Update simplify for initial testing.

--- ci/fortify.sh
+++ ci/fortify.sh
@@ -3,6 +3,4 @@
 sh "echo scanning" 
 # Fortify Scan
 sh "/opt/hp_fortify_sca/bin/sourceanalyzer -b ${env.BUILD_NUMBER} "../src/main/" 
-#sh "/opt/hp_fortify_sca/bin/sourceanalyzer -b ${env.BUILD_NUMBER}  -scan -f fortifyResults-${env.BUILD_NUMBER}.fpr" 
 
-#sh "/bin/curl -v --insecure -H 'Accept: application/json' -X POST --form file=@fortifyResults-${env.BUILD_NUMBER}.fpr https://threadfix.devops.geointservices.io/rest/applications/1/upload?apiKey=${threadfixApiKey}"
