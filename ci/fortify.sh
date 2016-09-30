84b6f894341610a98fb4e0d8dbaea0a70dbf75a8
Ben Hosmer <ben.hosmer@gmail.com>
2016-09-30 11:11:44 -0400

    Fix syntax error

--- ci/fortify.sh
+++ ci/fortify.sh
@@ -2,5 +2,5 @@
 
 sh "echo scanning" 
 # Fortify Scan
-sh "/opt/hp_fortify_sca/bin/sourceanalyzer -b ${env.BUILD_NUMBER} "../src/main/" 
+sh "/opt/hp_fortify_sca/bin/sourceanalyzer -b ${env.BUILD_NUMBER}" "../src/main/" 
 
