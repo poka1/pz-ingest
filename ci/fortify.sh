4362b92c0857f4b4bc925bf80c3a921881d49610
Ben Hosmer <ben.hosmer@gmail.com>
2016-10-03 09:02:51 -0400

    Try scanning just one file because of low slave resources.

--- ci/fortify.sh
+++ ci/fortify.sh
@@ -5,7 +5,7 @@ root=$(pwd -P)
 echo scanning 
 # Fortify Scan
 echo 'Building' $BUILD_NUMBER
-/opt/hp_fortify_sca/bin/sourceanalyzer -b $BUILD_NUMBER $root/src/main
+/opt/hp_fortify_sca/bin/sourceanalyzer -b $BUILD_NUMBER $root/src/main/java/ingest/Application.java
 /opt/hp_fortify_sca/bin/sourceanalyzer -b $BUILD_NUMBER -scan -f fortifyResults-$BUILD_NUMBER.fpr 
 cat fortifyResults-$BUILD_NUMBER.fpr 
 #sh "/bin/curl -v --insecure -H 'Accept: application/json' -X POST --form file=@fortifyResults-${env.BUILD_NUMBER}.fpr https://threadfix.devops.geointservices.io/rest/applications/1/upload?apiKey=${threadfixApiKey}"
